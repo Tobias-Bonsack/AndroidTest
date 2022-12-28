96aec11a346d5c24044cccf127460de1c7a0d988
Tobias-Bonsack <tobias.bonsack@gmail.com>
2022-12-28 20:51:49 +0100

    add lucky number module

---
+++ LuckyNumbers/src/main/java/com/example/luckynumbers/LnMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.luckynumbers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LnMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ln_main);
+    }
+}
--- app/src/main/java/com/example/androidtest/MainActivity.java
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.luckynumbers.LnMainActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,6 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.intentBtn).setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), MainActivity2.class)));
+        findViewById(R.id.intentBtn).setOnClickListener(view ->
+                startActivity(new Intent(getApplicationContext(), MainActivity2.class))
+        );
+
+        findViewById(R.id.luckyNumberBtn).setOnClickListener(view ->
+                startActivity(new Intent(getApplicationContext(), LnMainActivity.class))
+        );
+
     }
 }
